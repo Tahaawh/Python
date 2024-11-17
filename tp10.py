fa3107d16c7dc0fbf23f35042fad8d4aa219c4e9
Tahaawh <166891858+Tahaawh@users.noreply.github.com>
2024-11-17 16:03:23 +0330

    Update tp10.py

--- tp10.py
+++ tp10.py
@@ -12,3 +12,11 @@ def backup_file(source_file, backup_file):
 source = "file1.txt"
 backup = "backup_file1.txt"
 backup_file(source, backup)
+
+
+riazi=20 shimi=17
+hendeseh=18.5
+arabi-15
+farsi=19
+varzesh=20
+english=18
